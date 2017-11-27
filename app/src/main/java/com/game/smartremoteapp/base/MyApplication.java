4bdbbcf72975eaecc6b9c001b4042d940f5e7b48
zhouhua <zhouhua086@163.com>
2017-11-27 12:00:42 +0800

    crash handler /game_ha branch

--- app/src/main/java/com/game/smartremoteapp/base/MyApplication.java
+++ app/src/main/java/com/game/smartremoteapp/base/MyApplication.java
@@ -27,6 +27,10 @@ public class MyApplication extends MultiDexApplication {
         videoCtrlSettings();
         startCoreService();
         getPushAgent();
+        setCrashHandler();
+    }
+
+    private void setCrashHandler() {
 
     }
 
